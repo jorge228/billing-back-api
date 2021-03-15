e5081392c3a4f27b43e455ac3a32c7cce0fe3351
jorge <jorgelozano228@gmail.com>
2021-03-15 16:22:40 +0100

    handler update

--- src/main/java/com/lozano/billing/back/api/controllers/ClientRestController.java
+++ src/main/java/com/lozano/billing/back/api/controllers/ClientRestController.java
@@ -15,7 +15,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.lozano.billing.back.api.models.entity.Client;
@@ -35,6 +34,7 @@ public class ClientRestController {
 		map.put("success", true);
 		map.put("data", clientService.findAll());
 		map.put("message", "Get all clients.");
+		// TODO count
 		return map;
 	}
 
@@ -48,46 +48,66 @@ public class ClientRestController {
 		try {
 			client = clientService.findById(id);
 		} catch (DataAccessException e) {
+			map.put("success", false);
 			map.put("message", "Error query in database.");
 			map.put("error", e.getMessage().concat(": ").concat(e.getMostSpecificCause().getMessage()));
 			return new ResponseEntity<Map<String, Object>>(map, HttpStatus.INTERNAL_SERVER_ERROR);
 		}
 
 		if (client == null) {
+			map.put("success", false);
 			map.put("message", "Client with " + id + " doesn't exist.");
 			return new ResponseEntity<Map<String, Object>>(map, HttpStatus.NOT_FOUND);
 		}
-
+		// TODO return map with client
 		return new ResponseEntity<Client>(client, HttpStatus.OK);
 	}
 
 	@PostMapping("/clients")
-	//@ResponseStatus(HttpStatus.CREATED)
+	// @ResponseStatus(HttpStatus.CREATED)
 	public ResponseEntity<?> create(@RequestBody Client client) {
-		
+
 		Client clientNew = null;
 		Map<String, Object> map = new HashMap<>();
-		
+
 		try {
 			clientNew = clientService.save(client);
 		} catch (DataAccessException e) {
+			map.put("success", false);
 			map.put("message", "Error creating new client.");
 			map.put("error", e.getMessage().concat(": ").concat(e.getMostSpecificCause().getMessage()));
 			return new ResponseEntity<Map<String, Object>>(map, HttpStatus.INTERNAL_SERVER_ERROR);
 		}
-		
+
 		map.put("success", true);
 		map.put("data", clientNew);
 		map.put("message", "Client was created successfully.");
-		
+
 		return new ResponseEntity<Map<String, Object>>(map, HttpStatus.CREATED);
 	}
 
 	@PutMapping("/clients/{id}")
-	@ResponseStatus(HttpStatus.CREATED)
-	public Client update(@RequestBody Client client, @PathVariable Long id) {
-		client.setId(id);
-		return clientService.save(client);
+	// @ResponseStatus(HttpStatus.CREATED)
+	public ResponseEntity<?> update(@RequestBody Client client, @PathVariable Long id) {
+
+		Client clientUpdated = null;
+		Map<String, Object> map = new HashMap<>();
+
+		try {
+			client.setId(id);
+			clientUpdated = clientService.save(client);
+		} catch (DataAccessException e) {
+			map.put("success", false);
+			map.put("message", "Error updating client.");
+			map.put("error", e.getMessage().concat(": ").concat(e.getMostSpecificCause().getMessage()));
+			return new ResponseEntity<Map<String, Object>>(map, HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+
+		map.put("success", true);
+		map.put("data", clientUpdated);
+		map.put("message", "Client was updated successfully.");
+
+		return new ResponseEntity<Map<String, Object>>(map, HttpStatus.CREATED);
 	}
 
 	@DeleteMapping("/clients/{id}")
